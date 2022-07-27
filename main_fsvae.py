a0ea4462ddaccb0128de22386722c28fcf79adc4
Gintautas Palinauskas <palinauskas@fortiss.org>
2022-07-27 12:41:23 +0200

    reduced augmentations, reduce on plataue 2

--- main_fsvae.py
+++ main_fsvae.py
@@ -91,7 +91,6 @@ def train(network, trainloader, opti, epoch):
         mean_sampled_z = (sampled_z.mean(0).detach().cpu() + batch_idx * mean_sampled_z) / (batch_idx+1) # (C,T)
 
         print(f'Train[{epoch}/{max_epoch}] [{batch_idx}/{len(trainloader)}] Loss: {loss_meter.avg}, RECONS: {recons_meter.avg}, DISTANCE: {dist_meter.avg}')
-        writer.add_scalar('Train/running_learning_rate', opti.lr, batch_count + batch_idx)
         writer.add_scalar('Train/running_loss', loss_meter.avg, batch_count + batch_idx)
         writer.add_images('Train/running_input_img', (real_img+1)/2, batch_count + batch_idx)
         writer.add_images('Train/running_recons_img', (x_recon+1)/2, batch_count + batch_idx)
@@ -347,7 +346,9 @@ if __name__ == '__main__':
             logging.info("update p")
         train_loss = train(net, train_loader, optimizer, e)
         test_loss = test(net, test_loader, e)
-        # scheduler.step(train_loss)
+
+        writer.add_scalar('Train/running_learning_rate', scheduler.get_lr(), e)
+        scheduler.step(train_loss)
 
         torch.save(net.state_dict(), f'checkpoint/{args.name}/checkpoint.pth')
         if test_loss < best_loss:
