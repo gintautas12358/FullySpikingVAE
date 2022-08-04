8952cc661998cab3f506a3d514aab6b8a7758fae
Gintautas Palinauskas <palinauskas@fortiss.org>
2022-08-04 12:50:53 +0200

    rerun script

---
+++ run_script2_rerun.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+#SBATCH --gres=gpu:1
+
+echo NetworkConfigs/hole$1.yaml
+echo
+cat NetworkConfigs/hole$1.yaml
+
+sudo singularity exec  singularity/fsvae.sif python3 main_fsvae.py hole_run$1 -config NetworkConfigs/hole$1.yaml -checkpoint checkpoint/hole_run$1/checkpoint.pth
+
